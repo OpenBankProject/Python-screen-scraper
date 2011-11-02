5f60d11dcc35b0609fbba4aac00a41f8ac37573a
Akendo <4k3nd0@gmail.com>
2011-11-02 17:54:18 +0100

    Add some comments, add __author__,__license__,__doc__

--- OBP_Import/obp_postbank_get_csv.py
+++ OBP_Import/obp_postbank_get_csv.py
@@ -1,3 +1,21 @@
+__author__ = [' Jan Alexander Slabiak (alex@tesobe.com)']
+__license__ = """
+  Copyright 2011 Music Pictures Ltd / TESOBE
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
@@ -5,8 +23,7 @@ from os import getcwd
 import time
 
 
-
-'''
+__doc__='''
 Using Selenium RC2, install it with "pip install selenium"
 need also =>selenium-2.0
 
@@ -34,9 +51,17 @@ browser.get("https://banking.postbank.de/rai/login") # Load page
 assert "Postbank Online-Banking" in browser.title
 time.sleep(1)
 
+# This will a simple login to the Demo APP of PostBank
+# When we need to add some login infomation, we need to find the input fields.
+# LINK: http://seleniumhq.org/docs/03_webdriver.html#getting-started-with-selenium-webdriver
+# J.A.S
+
 browser.get("https://banking.postbank.de/rai/login/wicket:interface/:0:login:demokontoLink::ILinkListener::")
 browser.get("https://banking.postbank.de/rai/?wicket:bookmarkablePage=:de.postbank.ucp.application.rai.fs.UmsatzauskunftPage")
 browser.get("https://banking.postbank.de/rai/?wicket:interface=:2:umsatzauskunftpanel:panel:umsatzanzeigeGiro:umsatzaktionen:csvHerunterladen::IResourceListener::&")
 time.sleep(1)
+
+# TODO: Adding some test and also check for the downloaded file.
+
 browser.close()
 
