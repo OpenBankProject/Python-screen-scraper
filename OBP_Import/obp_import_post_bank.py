24cf738cb3fc214a2a6a2faefa4a1f2b8ec86b29
Akendo <4k3nd0@gmail.com>
2011-11-03 15:27:09 +0100

    add some doc comments

--- OBP_Import/obp_import_post_bank.py
+++ OBP_Import/obp_import_post_bank.py
@@ -22,7 +22,8 @@ import codecs
 import bson
 import os
 import datetime
-
+import re
+import json
 
 from socket import gethostname
 from pymongo import Connection
@@ -31,20 +32,6 @@ from bson import json_util
 
 
 '''
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myapp.settings'
-
-from django.core.serializers.json import DjangoJSONEncoder
-
-class MongoAwareEncoder(DjangoJSONEncoder):
-    """JSON encoder class that adds support for Mongo objectids."""
-    def default(self, o):
-        if isinstance(o, objectid.ObjectId):
-            return str(o)
-        else:
-            return super(MongoAwareEncoder, self).default(o)
-
-
-
 def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
     # csv.py doesn't do Unicode; encode temporarily as UTF-8:
     csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
@@ -58,33 +45,18 @@ def utf_8_encoder(unicode_csv_data):
         yield line.encode('utf-8')
 '''
 
-def get_replace_dot(dotted_input):
-    #The first Dot has beeing replaced
-    pass
-
-def get_numbers(input_number):
-    
-    pass
-
 
 def do_import():
-    import re
-    import json
     print ('starting import')
+
     connection = Connection('obp_mongod', 27017)
-    db = connection.OBP004
+    db = connection.OBP004 # Name of the Database with connecting to. 
 
+    # Alex Path
+    csv_path = '/home/akendo/PB_Umsatzauskunft_198_rows.csv'
 
-    #csv_path = '/home/akendo/PB_Umsatzauskunft_198_rows.csv'
-
-
-    csv_path = '/Volumes/not_on_your_nelly/Bank_statements/PB_Umsatzauskunft_KtoNr0580591101_04-10-2011_1624_saved.csv'
-
-
-
-
-
-
+    # Simons Path
+    #csv_path = '/Volumes/not_on_your_nelly/Bank_statements/PB_Umsatzauskunft_KtoNr0580591101_04-10-2011_1624_saved.csv'
 
     delimiter = ';'
     quote_char = '"'
@@ -92,40 +64,19 @@ def do_import():
     # Need a header check, so to make sure that only transaction data get insert
     # best would be a test for a vaild date format. 
     # re : \d\d\.\d\d\.\d\d\d\d
+    # This will check of date formarted lile this: 23.01.2001
     data_expression = re.compile('\d\d\.\d\d\.\d\d\d\d')
-
-    
     transactionReader = csv.reader(open(csv_path, 'rb'), delimiter=delimiter, quotechar=quote_char)
 
-
-
-
     for row in transactionReader:
-
-
-        print 'row is %s ' % row
-
-
-        print 'row[0] is %s ' % row[0]
-
-
         # The first vaild entry has always a date, checking for it
         if data_expression.match(row[0]) == None:
             continue
         else:
-            """
-            obp_transaction_row = {  'obp_transaction_date_start': row[0]
-                                    ,'obp_transactions_date_complete': row[1]
-                                    ,'get_obp_transaction_type_de': row[2]
-                                    ,'obp_transaction_data_blob': row[5]
-                                    ,'obp_transaction_amount':row[6]
-                                    ,'obp_transaction_new_balance':row[7]}
-       
-            print type(obp_transaction_row)
-            """
-            #print obp_transaction_row
             # Will now formating obp_string to json
-            #import pdb;pdb.set_trace() 
+            # We reading a Unicode File. That makes the € to a /xasd, 
+            # this regual expression search for all kind of Numbers in a string.
+            # Also covering + and - 
             amount = re.match("[+-]?((\d+(\.\d*)?)|\.\d+)([eE][+-]?[0-9]+)?", row[6])
             new_balance = re.match("[+-]?((\d+(\.\d*)?)|\.\d+)([eE][+-]?[0-9]+)?", row[7])
             obp_transaction_json = json.dumps({
@@ -137,9 +88,12 @@ def do_import():
                                      ,u'obp_transaction_data_blob': row[5]
                                      ,u'obp_transaction_amount': amount.group()
                                      ,u'obp_transaction_new_balance': new_balance.group()
-                             }, separators=(',',':'), default=bson.json_util.default)#, sort_keys=True, indent=4)
+                             }, separators=(',',':'), default=bson.json_util.default) indent=4)
             
-            #obp_transaction_json =  3
+            # Will call a dic which get decode from the BSON, so we can insert
+            # the single elements not as unicode string. 
+            # We inserting also some Information like time(even with tz)
+            # and hostname, this should change later to Public IP etc...
             posting = son.SON({
                      'bank_account':1234567
                     ,'uploader_host': gethostname()
@@ -147,9 +101,14 @@ def do_import():
                     ,'obp_transation': obp_transaction_json
                     })
 
-        #import pdb;pdb.set_trace()
-        print "In the JSON is:\n%s" % posting
+        # This will print the no binary JSON, that get insert to mongodb
+        # TODO: Need to create Uniq Indexes.
+        # To ensure that a Transaction is always uniq
+        # LINK: http://www.mongodb.org/display/DOCS/Indexes#Indexes-UniqueIndexes
+        print "In the JSON is:\n%s" % posting 
+        # Inserting the finisch JSON to the collection 
         collection = db.obptransactions.insert(posting)
+
 
 
 if __name__ == '__main__':
