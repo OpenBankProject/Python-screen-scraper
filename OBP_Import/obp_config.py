d207d671437dd941e110057252f6438b8ed2b3a1
Alexander <alex@tesobe.com>
2011-11-08 18:47:32 +0100

    Add some MongoDB options

--- OBP_Import/obp_config.py
+++ OBP_Import/obp_config.py
@@ -3,7 +3,14 @@
 MONGODB_SERVER='obp_mongod'
 MONGODB_SERVER_PORT='27017'
 
+#MongoDB Setting
+MONGODB_DATABASE='OBP005'
+MONGODB_COLLECTION='obptransactions'
+
+
 # Files:
 # The Folder where the CSV file get saved
 SAVEDIR=''
 TMP='/tmp/'
+
+
