000ec553d5c2c8661ad03b4c36491b4810b01716
Alexander <alex@tesobe.com>
2011-11-21 17:22:52 +0100

    Add some confs

--- OBP_Import/obp_config.py
+++ OBP_Import/obp_config.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Server Settings:
 MONGODB_SERVER='obp_mongod'
-MONGODB_SERVER_PORT='27017'
+MONGODB_SERVER_PORT=27017
 
 #MongoDB Setting
-MONGODB_DATABASE='OBP005'
+MONGODB_DATABASE='OBP006'
 MONGODB_COLLECTION='obptransactions'
 
 
