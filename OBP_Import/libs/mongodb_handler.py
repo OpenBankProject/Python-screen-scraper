53c2a2dfcdee0bebf0f8a9524167c42e86ecd05a
Alexander <alex@tesobe.com>
2011-11-24 18:31:42 +0100

    Adding mongodb_handler, take care about mongodb connection, etc.

--- OBP_Import/libs/mongodb_handler.py
+++ OBP_Import/libs/mongodb_handler.py
@@ -3,9 +3,32 @@ __doc__ = '''
 This is the mongodb_handler
 '''
 from pymongo import Connection
+import bson.son
 
-def insert_into_mongodb(data_to_insert):
-    collection = db.obptransactions.insert(data_to_insert)
-    return collection
 
+def connect_to_mongod(mongdod_host,mongod_host_port):
+    mongod_connection = Connection(mongdod_host,mongod_host_port)
+    return mongod_connection
+
+
+def connect_to_mongod_db(connection,mongod_db):
+    mongodb = connection.mongod_db
+    # Looks like that he is not resolinv the incoming varriable in the right
+    # way. They keep beeing unresolve and insert to the mongod_db.
+    # Need a fix!
+    return mongodb
+
+def mongodb_to_collection(mongodb,mongo_collection):
+    mongodb_collection = mongodb.mongo_collection
+    return mongodb_collection
+
+
+def insert_into_mongodb(collection,data_to_insert):
+    object_id = collection.insert(data_to_insert)
+    return object_id
+
+
+def pre_insert_data(data_to_prepere):
+    #This function will convert the data to son.
+    pass
 
