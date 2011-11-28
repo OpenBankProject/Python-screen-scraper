19d6bd96c3e8b7ae050e56a702ee46ed20c9811b
Alexander <alex@tesobe.com>
2011-11-28 18:07:45 +0100

    Add some function for smooth importing

--- OBP_Import/main.py
+++ OBP_Import/main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
+import os
 import obp_postbank_get_csv
 import obp_import_post_bank
-import os
+import libs.to_utf8.py
+
+
 
 csv_file = os.path.join(obp_postbank_get_csv.csv_save_path,obp_postbank_get_csv.csv_files[0])
 
-print csv_file
+libs.to_utf8.main(csv_file)
 
 # This no working right now! 
+
 obp_import_post_bank.main(csv_file)
 
 
