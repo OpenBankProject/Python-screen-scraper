21b4e966383cc99d79a6a12f4d2510a2da9d7cf9
Alexander <alex@tesobe.com>
2011-11-28 18:06:17 +0100

    Add some docs, clean up some files, start working with class obp_transation, work with method time_convert

---
+++ OBP_Import/libs/transactions.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import obp_config
+
+import datetime
+from socket import gethostname
+
+class obp_account:
+    OBP_version = obp_config.OBP_VERSION
+    OBP_loader_host = gethostname()
+    insert_date = datetime.datetime.utcnow()
+
+
+
+
+class obp_transaction:
+    start_date = ''
+    complete_date = ''
+    transaction_type = ''
+    comment_1 = ''
+    comment_2 = ''
+    data_blob = ''
+    amount = ''
+    new_balance = ''
+    this_account  = ''
+    source_bank = ''
+
+
+
+    def check_date_format(self,date_to_check):
+        # This will change, currently is the default import date in german 
+        # time format of DD.MM.YYYY
+        try:
+            valid_date = datetime.datetime.strptime( date_to_check, "%d.%m.%Y" )
+            return True
+        except ValueError:
+              print('Invalid date!')
+              return False
+
+    def convert_date_to_clean_date(self,date_to_convert):
+        # This function will convert a Date to a UTC time. 
+        # It will take an input and return it as a UTC.
+        assert check_date_format(date_to_convert)
+
+        # The date only showing the detail of the day. Setting Sec,Min to 0
+        t = time(0, 0)
+        # We now where here the date is: 
+
+        self.to_convert = datetime.date(date_to_convert[6:9],date_to_convert[3:4],
+                date_to_convert[0:1])
+        datetime.combin(self.to_convert, t)
+        datetime.datetime.strftime(self.to_convert, "%Y,%m,%d,%M,%S")
+
+        # Untest!
+
+
+'''
+u'obp_transaction_date_start': input_row[0]
+u'obp_transaction_date_complete':input_row[1]
+u'obp_transaction_transaction_type_de':input_row[2]
+u'obp_transaction_comment1': input_row[3].rstrip()
+u'obp_transaction_comment2': input_row[4].rstrip()
+u'obp_transaction_data_blob': input_row[5]
+u'obp_transaction_amount': amount.group()
+u'obp_transaction_new_balance': new_balance.group()
+'''
