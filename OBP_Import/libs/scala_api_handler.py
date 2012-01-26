e9ac6047f6de4e30b00c6fb7a27cd0d98ecd026e
Alexander Slabiak <alex@tesobe.com>
2012-01-26 15:43:36 +0100

    Update license text to 2011/2012

--- OBP_Import/libs/scala_api_handler.py
+++ OBP_Import/libs/scala_api_handler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 __author__ = ['Jan Alexander Slabiak (alex@tesobe.com)']
 __license__ = """
-  Copyright 2011 Music Pictures Ltd / TESOBE
+  Copyright 2011/2012 Music Pictures Ltd / TESOBE
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
--- OBP_Import/main.py
+++ OBP_Import/main.py
@@ -3,7 +3,7 @@
 
 __author__ = [' Jan Alexander Slabiak (alex@tesobe.com)']
 __license__ = """
-  Copyright 2011 Music Pictures Ltd / TESOBE
+  Copyright 2011/2012 Music Pictures Ltd / TESOBE
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
