e67e35cb964c055058ccb79b5ae1fe3f12b7a29d
Alexander Slabiak <alex@tesobe.com>
2012-08-30 20:18:52 +0200

    Fixed GLS Login, removed debug() functions.
    Problem with GLS Login was caused by calling submit()function of Selenium.
    It couldn't find the login but. Removed it.

--- OBP_Import/libs/gls_get_csv.py
+++ OBP_Import/libs/gls_get_csv.py
@@ -42,8 +42,8 @@ from debugger import obp_logger
 from selenium.common.exceptions import (NoSuchElementException,
     ElementNotVisibleException)
 
-
-gls_main_url_login_page = "https://internetbanking.gad.de/ptlweb/WebPortal?bankid=4967&modus=demo"
+#gls_main_url_login_page = "https://internetbanking.gad.de/ptlweb/WebPortal?bankid=4967&modus=demo"
+gls_main_url_login_page = "https://internetbanking.gad.de/ptlweb/WebPortal?bankid=4967"
 gls_main_url_value_page = "https://internetbanking.gad.de/ptlweb/WebPortal?timeout=4967&applId=obaban&frame=content&wp_token_obaban=1364634655&event=anzeigenKontoUmsatzUebersicht&idEuroKontoGewaehlt=0"
 gls_main_url_value_download = "https://internetbanking.gad.de/ptlweb/WebPortal?timeout=4967&applId=obaban&frame=content&wp_token_obaban=291125145&event___export=Link"
 
@@ -109,14 +109,14 @@ def gls_get_csv_with_selenium(path_to_save_csv, username, password):
 
     # send Username and Password
     obp_logger.debug("Inserting username into login box: %s " % username)
-    #inputElement_username.send_keys(username)
-    inputElement_username.send_keys("6666700")
+    inputElement_username.send_keys(username)
+    #inputElement_username.send_keys("6666700")
     obp_logger.debug("Inserting password into login box")
-    inputElement_password.send_keys("13579")
+    inputElement_password.send_keys(password)
+    #inputElement_password.send_keys("13579")
 
     # submit the Username and Password to GLS.
     obp_logger.info("submitting login_data to login")
-    inputElement_password.submit()
 
     # Find login button, submit() function is not working, using click() instead.
     login_button = browser.find_element_by_class_name("gad-button-bg-left")
